81e0b44b48fd5b76ce649c75596a15ba6cc5e1c5
vntero <shin.antero@gmail.com>
2022-03-25 15:58:24 +0100

    added new_task and worker

---
+++ new_task.ts
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+var amqp: any = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function(error0: any, connection: any) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function(error1: any, channel: any) {
+        if (error1) {
+            throw error1;
+        }
+        var queue: string = 'task_queue';
+        var msg: any = process.argv.slice(2).join(' ') || "Hello World!";
+
+        channel.assertQueue(queue, {
+            durable: true
+        });
+        channel.sendTOQueue(queue, Buffer.from(msg), {
+            persistent: true
+        });
+        console.log(" [x] Sent '%s'", msg);
+    });
+    setTimeout(function(){
+        connection.close();
+        process.exit(0)
+    }, 500);
+});
+
+
+
